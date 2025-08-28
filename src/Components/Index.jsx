7112b652899a49f7841073de6925759ab536f6c6
SonikMehar8817 <144803617+SonikMehar8817@users.noreply.github.com>
2025-08-28 22:31:42 +0530

    Update Index.jsx

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -2,39 +2,6 @@ import React, { useState } from "react";
 import data from "./Data.js";
 
 const Index = () => {
-  //     const [cheakId, setCheakId] = useState([]);
-  //     var value = 0;
-  //     const handleClick =(dataId)=>{
-  //         value = dataId;
-  //         // setCheakId(cheakId.includes(dataId)?cheakId.filter(removeElm):cheakId.push(dataId))
-  //         cheakId.push(dataId)
-  //     }
-  //     const isTrue = (el) =>{
-  //         for(let i=0;i<cheakId.length;i++){
-  //             if(cheakId[i]==el) return true
-  //         }
-  //         return false;
-  //     }
-  //     console.log(cheakId);
-
-  //   return <div className="container">
-  //     <button onClick={()=>enbaleMultiSel()}>Enable Multi Selection</button>
-  //     <div className="main">
-  //          {
-  //         data.map((item)=><div className="items" id={item.id} >
-  //             <div className="box" >
-
-  //                 <div onClick={()=>handleClick(item.id)}>
-  //                     <h3>Q. {item.question}</h3>
-  //                 </div>
-  //                 <div> {isTrue(item.id)&&<h4>A. {item.answer}</h4>}</div>
-  //             </div>
-  //         </div>)
-  //       }
-  //     </div>
-
-  //   </div>;
-
   const [cheakId, setCheakId] = useState(null);
   const [enableMultiSel, setEnableMultiSel] = useState(false);
   const [multiple, setMultiple] = useState([]);
